2da8e389ea8ec7453cfbbfcf99d09f744c75f403
Zaafir Rahil <zaafir909@gmail.com>
2024-06-10 10:57:00 +0200

    added Method outside Main Function and add logic spacing

--- BasicCalculatorC#P24/Program.cs
+++ BasicCalculatorC#P24/Program.cs
@@ -8,25 +8,37 @@ namespace testC_P24
 {
     internal class Program
     {
+
+        static void Answer(string Number) {
+            Console.WriteLine(("Answer:" + Number + "\n"));
+        }
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Enter First Number:");
             int First = Convert.ToInt32(Console.ReadLine());
+
             Console.WriteLine("Enter Second Number:");
             int second = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("Enter Operation(1 - addition, 2 - Subtraction, 3 - Multiplication , 4- Division,5 - Quite):");
             string operation = Console.ReadLine();
+
+
             while (operation != "5")
             {
-                if (operation == "1") { Console.WriteLine("Answer:" + (First + second)+"\n"); }
-                else if (operation == "2") { Console.WriteLine("Answer:"+(First - second) + "\n"); }
-                else if (operation == "3") { Console.WriteLine("Answer:" + (First * second) + "\n"); }
-                else if (operation == "4") { Console.WriteLine("Answer:" + ((double)First / (double)second).ToString("F") + "\n"); }
+                if (operation == "1") { Answer(Convert.ToString(First + second)); }
+
+                else if (operation == "2") { Answer(Convert.ToString(First - second)); }
+
+                else if (operation == "3") { Answer(Convert.ToString(First * second)); }
+
+                else if (operation == "4") { Answer(((double)First / (double)second).ToString("F")); }
                 else if (operation == "5") { break; }
 
                 Console.WriteLine("Enter First Number:");
                 First = Convert.ToInt32(Console.ReadLine());
+
                 Console.WriteLine("Enter First Number:");
                 second = Convert.ToInt32(Console.ReadLine());
 
